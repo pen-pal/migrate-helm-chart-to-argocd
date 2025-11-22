e5bb71622b54b77962992a6964bf4c7b753a70f6
pen-pal <unameme@protonmail.com>
2025-11-22 22:20:14 +0545

    fix bootstrap

--- bootsrap.sh
+++ bootsrap.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-REGION=$1
-PROFILE=$2
-BUCKET_NAME=${3:-terraform-state}
+BUCKET_NAME=${1:-terraform-state}
 
-aws cloudformation create-stack --region $REGION --profile $PROFILE --stack-name Terraform-State-Resources --enable-termination-protection --template-body file://terraform-bootstrap.cfn --parameters ParameterKey=TerraformStateBucketPrefix,ParameterValue=$BUCKET_NAME ParameterKey=TerraformStateLockTableName,ParameterValue=terraform-state-locks
+aws cloudformation create-stack  --stack-name Terraform-State-Resources --enable-termination-protection --template-body file://terraform-bootstrap.cfn --parameters ParameterKey=TerraformStateBucketPrefix,ParameterValue=$BUCKET_NAME ParameterKey=TerraformStateLockTableName,ParameterValue=terraform-state-locks
