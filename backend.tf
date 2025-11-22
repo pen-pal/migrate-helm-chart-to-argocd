8ea11964fbe96545fc7ee4e61e8caa918a051d05
pen-pal <unameme@protonmail.com>
2025-11-22 23:50:08 +0545

    moved to infra folder

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-us-east-1-demo-argocd-gitops"
-    key    = "state/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- plugin-variables.tf
+++
@@ -1,5 +0,0 @@
-variable "create_argocd" {
-  description = "either to create and deploy kube prometheus strack resources"
-  type        = bool
-  default     = false
-}
